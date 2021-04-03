a415d6eca1b434a016aca29e048518885bb2fbf2
Eli Aloni <elix22@gmail.com>
2021-04-03 12:57:52 +0300

    Update Sample.cs

--- CrowdNavigation/Source/Sample.cs
+++ CrowdNavigation/Source/Sample.cs
@@ -83,7 +83,7 @@ namespace CrowdNavigation
 
 		protected override void Start ()
 		{
-			Log.LogMessage += e => Debug.WriteLine($"[{e.Level}] {e.Message}");
+			
 			base.Start();
 			if (Platform == Platforms.Android || 
 				Platform == Platforms.iOS || 
