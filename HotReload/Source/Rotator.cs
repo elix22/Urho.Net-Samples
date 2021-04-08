353e86d32fdbf5dfdfb0d76e2afab19b6288bd1d
Eli <elix22@gmail.com>
2021-04-08 18:41:58 +0300

    HotReload - crash fix , check Node != null

--- HotReload/Source/Rotator.cs
+++ HotReload/Source/Rotator.cs
@@ -22,20 +22,20 @@ namespace HotReload
         }
 
 
-    	public override void OnDeserialize(IComponentDeserializer d)
-		{
-			RotationSpeed = d.Deserialize<Vector3>(nameof(RotationSpeed));
+        public override void OnDeserialize(IComponentDeserializer d)
+        {
+            RotationSpeed = d.Deserialize<Vector3>(nameof(RotationSpeed));
             Node.Position = d.Deserialize<Vector3>(nameof(Node.Position));
             Node.Rotation = d.Deserialize<Quaternion>(nameof(Node.Rotation));
-		}
+        }
 
-		public override void OnSerialize(IComponentSerializer s)
-		{
-			s.Serialize(nameof(RotationSpeed), RotationSpeed);
-            s.Serialize(nameof(Node.Position),  Node.Position);
-            s.Serialize(nameof(Node.Rotation),  Node.Rotation);
-           
-		}
+        public override void OnSerialize(IComponentSerializer s)
+        {
+            s.Serialize(nameof(RotationSpeed), RotationSpeed);
+            s.Serialize(nameof(Node.Position), Node.Position);
+            s.Serialize(nameof(Node.Rotation), Node.Rotation);
+
+        }
 
         public Rotator(IntPtr handle) : base(handle)
         {
@@ -53,11 +53,14 @@ namespace HotReload
 
         protected override void OnUpdate(float timeStep)
         {
-            Node.Rotate(new Quaternion(
-                RotationSpeed.X * timeStep,
-                RotationSpeed.Y * timeStep,
-                RotationSpeed.Z * timeStep),
-                TransformSpace.Local);
+            if (Node != null)
+            {
+                Node.Rotate(new Quaternion(
+                    RotationSpeed.X * timeStep,
+                    RotationSpeed.Y * timeStep,
+                    RotationSpeed.Z * timeStep),
+                    TransformSpace.Local);
+            }
         }
 
 
